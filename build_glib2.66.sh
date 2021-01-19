0b52b615954b8d16d00a7ee22a5e150a6cb4730f
tangfuyuan <362087583@qq.com>
2021-01-19 09:13:58 +0000

    Add supprot for meson build glib-2.66.4

--- build_glib2.66.sh
+++ build_glib2.66.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-# ./configure --host=arm-linux-androideabi --prefix=$PWD/../out/ \
-    # --includedir=/home/geek/work/ndk/android-ndk-r21b/sources/android/cpufeatures/
 
-	# --includedir $PWD/out/include \
-	# --libdir $PWD/out/lib \
-	# -Dlibdir=$PWD/out/lib \
-	# -Dincludedir=$PWD/out/include \
+. ./env.sh
 
-. env.sh
+NINJA_DIR=ninjadir
 
-OUTDIR=/home/geek/work/ndk/cairo/out/
-
-	# meson --reconfigure
-CC=arm-linux-androideabi-gcc CFLAGS="-I$OUTDIR/include -L$OUTDIR/lib" meson setup --wipe  glib-2.66.0 release/ \
-	--includedir $OUTDIR/include \
-	--libdir $OUTDIR/lib \
-	--cross-file cross_file.txt \
-	--prefix=$OUTDIR/ \
-	-Dc_arg="-I$OUTDIR/include -L$OUTDIR/lib" \
-	-Dc_link_args="-I$OUTDIR/include -L$OUTDIR/lib" \
-	-Diconv=external \
-	-Dinternal_pcre='true' \
+cd glib-2.66.4/ &&
+meson $NINJA_DIR --cross-file ../cross_file.txt &&
+cd $NINJA_DIR &&
+ninja &&
+ninja install
